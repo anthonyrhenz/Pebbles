ba02f8baff6ed47588068386ea8d4ae87495238d
Jakub Kwak <kubakwak123@gmail.com>
2019-11-16 13:49:37 +0000

    added more methods to bag class

--- PebbleGame/src/com/kwakbennett/pebblegame/model/Bag.java
+++ PebbleGame/src/com/kwakbennett/pebblegame/model/Bag.java
@@ -4,43 +4,53 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * A simple bag class (UNFINISHED/PLACEHOLDER)
+ * A simple bag class which contains a pebble lists
  */
 public class Bag {
-    private ArrayList<Pebble> pebbleList;
+    private ArrayList<Pebble> pebbles;
     private String name;
     private Random ran;
 
     public Bag() {
-        this.pebbleList = new ArrayList<>();
         this.ran = new Random();
+        this.pebbles = new ArrayList<>();
+    }
+
+    public Bag(ArrayList<Integer> pebbleValues) {
+        this.ran = new Random();
+        this.pebbles = new ArrayList<>();
+
+        for (int pebbleValue : pebbleValues) {
+            this.pebbles.add(new Pebble(pebbleValue));
+        }
     }
 
     public String getName() {
         return name;
     }
 
-    public ArrayList<Pebble> getPebbleList() {
-        return pebbleList;
-    }
-
-    public void setPebbleList(ArrayList<Pebble> pebbleList) {
-        this.pebbleList = pebbleList; //not really sure if we need this method if we have add pebbles
-    }
-
     /**
-     * Removes a random pebble from the object's pebbleList
-     * @return a random pebble from the pebbleList
+     * Removes a random pebble from the bags's pebble list
+     * @return a random pebble from the pebble list
      */
     public Pebble takeRandomPebble() {
-        return pebbleList.remove(ran.nextInt(pebbleList.size()));
+        return pebbles.remove(ran.nextInt(pebbles.size()));
     }
 
     /**
-     * Adds a list of pebbles to the object's pebbleList
+     * Adds a list of pebbles to the bag's pebble list
      * @param pebbleList an ArrayList containing pebbles
      */
     public void addPebbles(ArrayList<Pebble> pebbleList) {
-        this.pebbleList.addAll(pebbleList);
+        this.pebbles.addAll(pebbleList);
+    }
+
+    /**
+     * Takes the bag's pebbles and adds them to another bag
+     * @param bag bag to add the pebbles to
+     */
+    public void movePebbles(Bag bag) {
+        bag.addPebbles(pebbles);
+        pebbles.clear();
     }
 }
